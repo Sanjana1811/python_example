de9c377a67d70bee44e7a299e231f55bb6e403e5
sanjana1811 <sanjanasholka1899@gmail.com>
2025-04-12 20:35:04 -0700

    python basics

---
+++ python_practice_extra/Practice_questions/add.py
@@ -0,0 +1,4 @@
+num1 = float(input("enter the first number for addition: "))
+num2 = float(input("Enter the second number for addtion: "))
+sum_result = num1 + num2 
+print(f"sum: {num1} + {num2} = {sum_result}")
